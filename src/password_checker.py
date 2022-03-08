3b5a87db84dba816e25e29dab3cd67ea0dd86132
Wimmer, Tad <a675675@fmr.com>
2022-03-08 12:06:38 -0700

    Through length and lower case

--- src/password_checker.py
+++ src/password_checker.py
@@ -2,8 +2,30 @@
 
 class PasswordChecker:
 
-    def check_password(self, password):
+    @staticmethod
+    def check_password(password):
+        start = 'Password must'
+        message = PasswordChecker.check_length(password)
+        lower = PasswordChecker.check_lower_case(password)
+        if len(message) > 0 and len(lower) > 0:
+            message = message + ' and' + lower
+        elif len(message) == 0:
+            message = lower
+        if len(message) > 0:
+            return start + message
+
+    @staticmethod
+    def check_length(password):
         if len(password) < 8:
-            return 'Password must be 8 characters long'
+            return ' be 8 characters long'
         else:
             return ''
+
+    @staticmethod
+    def check_lower_case(password):
+        message = ' contain at least one lowercase letter'
+        for c in password:
+            if c.islower():
+                return ''
+        else:
+            return message
--- test/test_password_checker.py
+++ test/test_password_checker.py
@@ -4,8 +4,20 @@ from src.password_checker import PasswordChecker
 class TestPasswordChecker:
     def test_check_password_8_characters_fail(self):
         pc = PasswordChecker
-        assert pc.check_password(pc, 'P') == 'Password must be 8 characters long'
+        assert pc.check_password('p') == 'Password must be 8 characters long'
 
     def test_check_password_8_characters_pass(self):
         pc = PasswordChecker
-        assert pc.check_password(pc, 'asdfghjk') != 'Password must be 8 characters long'
+        assert pc.check_password('asdfghjk') != 'Password must be 8 characters long'
+
+    def test_check_password_lower_case_fail(self):
+        pc = PasswordChecker
+        assert pc.check_password("ABCDEFGH") == 'Password must contain at least one lowercase letter'
+
+    def test_check_password_all_pass(self):
+        pc = PasswordChecker
+        assert pc.check_password('Phyd3@ux') == None
+
+    def test_check_password_short_upper(self):
+        pc = PasswordChecker
+        assert pc.check_password("ABCDEFG") == 'Password must be 8 characters long and contain at least one lowercase letter'
