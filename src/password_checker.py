a646380ca8e1a884748a0abde9b0dac4dd1d54de
Wimmer, Tad <a675675@fmr.com>
2022-03-08 10:37:45 -0700

    chanaged error message for initial tests

--- src/password_checker.py
+++ src/password_checker.py
@@ -4,6 +4,6 @@ class PasswordChecker:
 
     def check_password(self, password):
         if len(password) < 8:
-            return 'Must be 8 characters long'
+            return 'Password must be 8 characters long'
         else:
             return ''
--- test/test_password_checker.py
+++ test/test_password_checker.py
@@ -4,8 +4,8 @@ from src.password_checker import PasswordChecker
 class TestPasswordChecker:
     def test_check_password_8_characters_fail(self):
         pc = PasswordChecker
-        assert pc.check_password(pc, 'P') == 'Must be 8 characters long'
+        assert pc.check_password(pc, 'P') == 'Password must be 8 characters long'
 
     def test_check_password_8_characters_pass(self):
         pc = PasswordChecker
-        assert pc.check_password(pc, 'asdfghjk') != 'Must be 8 characters long'
+        assert pc.check_password(pc, 'asdfghjk') != 'Password must be 8 characters long'
