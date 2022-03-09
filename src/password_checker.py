3c50631d0c1bcb2a9f16d2fda9fa2d6739d498b1
Wimmer, Tad <a675675@fmr.com>
2022-03-08 21:42:05 -0700

    All specs met and passing all tests

--- src/password_checker.py
+++ src/password_checker.py
@@ -1,21 +1,39 @@
 
-
 class PasswordChecker:
 
     @staticmethod
     def check_password(password):
-        start = 'Password must'
-        message = PasswordChecker.check_length(password)
+        message_list = list()
+        length = PasswordChecker.check_length(password)
         lower = PasswordChecker.check_lower_case(password)
         upper = PasswordChecker.check_uppercase(password)
-        if len(message) > 0 and len(lower) > 0:
-            message = message + ' and' + lower
-        elif len(message) == 0:
-            message = lower
-        if len(message) == 0 and len(upper) > 0:
-            return start + upper
-        if len(message) > 0:
-            return start + message
+        numeral = PasswordChecker.check_numeral(password)
+        symbol = PasswordChecker.check_symbol(password)
+        if len(length) > 0:
+            message_list.append(length)
+        if len(lower) > 0:
+            message_list.append(lower)
+        if len(upper) > 0:
+            message_list.append(upper)
+        if len(numeral) > 0:
+            message_list.append(numeral)
+        if len(symbol) > 0:
+            message_list.append(symbol)
+        return PasswordChecker.build_message(message_list)
+
+    @staticmethod
+    def build_message(message_list):
+        output = ''
+        count = len(message_list)
+        if count > 0:
+            output = 'Password must'
+            for i in range(0, count):
+                output += message_list[i]
+                if i < count - 2:
+                    output += ', '
+                elif i < count - 1:
+                    output += ', and'
+        return output if len(output) > 0 else None
 
     @staticmethod
     def check_length(password):
@@ -39,3 +57,18 @@ class PasswordChecker:
             if c.isupper():
                 return ''
         return message
+
+    @staticmethod
+    def check_numeral(password):
+        for c in password:
+            if c.isdigit():
+                return ''
+        return ' contain at least one numeral'
+
+    @staticmethod
+    def check_symbol(password):
+        symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '?']
+        for c in password:
+            if c in symbols:
+                return ''
+        return " contain a symbol in '!@#$%^&*()+?'"
